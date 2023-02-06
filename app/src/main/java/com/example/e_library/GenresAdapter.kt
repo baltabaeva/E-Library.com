29d553be5e1c3360d4f6a310ea69b372cf8b356f
baltabaeva <baltabaeva005@mail.ru>
2023-02-07 01:08:51 +0600

    Splash Screen

--- app/src/main/java/com/example/e_library/GenresAdapter.kt
+++ app/src/main/java/com/example/e_library/GenresAdapter.kt
@@ -3,12 +3,15 @@ package com.example.e_library
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.e_library.databinding.GenresBinding
 import com.example.e_library.model.Genres
+import kotlinx.android.synthetic.main.genres.view.*
 
 
 class GenresAdapter(private val onItemClicked: (Genres) -> Unit) :
@@ -16,13 +19,15 @@ class GenresAdapter(private val onItemClicked: (Genres) -> Unit) :
 
     private lateinit var context: Context
 
-    class GenresViewHolder(private var binding: GenresBinding) :
+
+    class GenresViewHolder(var binding: GenresBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(genres: Genres, context:Context) {
             binding.titleTv.text = context.getString(genres.titleResourceId)
             binding.logoIv.load(genres.imageResourceId)
         }
+        val cardView: CardView = itemView.findViewById(R.id.cardViewItem)
     }
 
     override fun onCreateViewHolder(
@@ -43,6 +48,8 @@ class GenresAdapter(private val onItemClicked: (Genres) -> Unit) :
             onItemClicked(current)
         }
         holder.bind(current, context)
+
+        holder.cardView.startAnimation(AnimationUtils.loadAnimation(holder.cardView.context,R.anim.animation))
     }
 
     companion object {
--- app/src/main/java/com/example/e_library/ui/genres/GenresFragment.kt
+++ app/src/main/java/com/example/e_library/ui/genres/GenresFragment.kt
@@ -1,5 +1,6 @@
 package com.example.e_library.ui.genres
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.SearchView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.e_library.GenresAdapter
 import com.example.e_library.data.GenresData
@@ -24,9 +26,6 @@ class GenresFragment : Fragment() {
 
     private val genresViewModel: GenresViewModel by activityViewModels()
 
-    //private lateinit var searchList: ArrayList<GenresData>
-
-//    EditText searchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,40 +52,9 @@ class GenresFragment : Fragment() {
         }
         binding.recyclerViewGenres.adapter = adapter
         adapter.submitList(genresViewModel.genresData)
-
-
-
-//        val searchView = binding.searchViewGenres
-//        val searchViewList = arrayListOf<GenresData>()
-//
-//        searchView.clearFocus()
-//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                searchView.clearFocus()
-//                return true
-//            }
-//
-//            override fun onQueryTextChange(newText: String?): Boolean {
-//                searchViewList.clear()
-//                val searchText = newText!!.toLowerCase(Locale.getDefault())
-//                if(searchText.isNotEmpty()){
-//                    ArrayList<Genres>().forEach(){
-//                        if (it.titleResourceId.toLowerCase(Locale.getDefault()).contains(searchText)){
-//                            searchList.add(it)
-//                        }
-//                    }
-//                    binding.recyclerViewGenres.adapter!!.notifyDataSetChanged()
-//                }else{
-//                    searchList.clear()
-//                    searchList.addAll(ArrayList<Genres>)
-//                    binding.recyclerViewGenres.adapter!!.notifyDataSetChanged()
-//                }
-//                return false
-//            }
-//
-//        })
    }
 }
+
 
 
 class GenresOnBackPressedCallback(
